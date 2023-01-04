c2ef5661b981f9604782efecd3f625912b48ee54
FearMyShotz <76791288+FearMyShotz@users.noreply.github.com>
2023-01-04 19:09:48 +0100

    ✨ Started work on CirelliumCore

---
+++ src/main/java/net/cirellium/core/CirelliumCore.java
@@ -0,0 +1,55 @@
+/**
+* Copyright (C) 2022 Cirellium Network - All Rights Reserved
+*
+* Created by FearMyShotz on Thu Dec 29 2022 17:22:04
+*
+* CirelliumCore.java is part of Cirellium Commons
+*
+* Unauthorized copying of this file, via any medium is strictly prohibited
+*/
+package net.cirellium.core;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import net.cirellium.commons.bukkit.CirelliumBukkitPlugin;
+import net.cirellium.commons.common.plugin.CirelliumPlugin;
+
+/**
+ * This is the main class for CirelliumCore.
+ * It manages all Cirellium plugins.
+ */
+public class CirelliumCore extends CirelliumBukkitPlugin<CirelliumCore> {
+
+    private static CirelliumCore instance;
+
+    private Set<CirelliumPlugin<?>> cirelliumPlugins;
+
+    @Override
+    public boolean load() {
+        this.cirelliumPlugins = new HashSet<CirelliumPlugin<?>>();
+
+        // TODO Setup NMS - if it fails, disable the plugin
+
+        return true;
+    }
+
+    @Override
+    public void enable() {
+        instance = this;
+    }
+
+    @Override
+    public void disable() {
+        
+    }
+
+    public static CirelliumCore getInstance() {
+        return instance;
+    }
+
+    @Override
+    public CirelliumCore getSelf() {
+        return this;
+    } 
+}
